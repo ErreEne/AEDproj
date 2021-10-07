00778c2d9f6fa715ac679fa6336f1a3fcec48003
Rodrigo Pereira <73695255+ErreEne@users.noreply.github.com>
2021-10-07 23:54:28 +0100

    saladas dum cabrao

--- main.c
+++ main.c
@@ -1,30 +1,157 @@
-//Projeto da tua mae de quatro
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
-int main(int argc, char *argv)
+    /*for (i = 0; i < c; i++)
+        tabuleiro[i] = (int*)calloc(c,sizeof(int));*/
+
+/*A6 = Se ponto de partida - branca ->inicializar a -2. 
+Correr algoritmo de procura e escolher uma peça branca. 
+Ponto de chegada? Não então colocar -2. Repetir processo até ponto de chegada. 
+Chega ao ponto de chegada, usar variavel auxiliar para saber que podem estar na mesma sala. Exit.*/
+
+void A1(int **tabuleiro, int l1i, int c1i, int li, int ci){
+    int valor = tabuleiro[l1i-1][c1i-1];
+    //ver se pertence ao tabuleiro
+    if(l1i-1 < 0 || c1i-1 < 0 || l1i > li || c1i>ci ){
+        //Não pertence no fodendo tabuleiro
+    }else{
+    
+    if(valor==-1){
+        //peça será preta
+        printf("está certa1");
+    }else if(valor==0){
+        //peça branca
+        printf("está certa");
+    }else if(valor>0){
+        //peça cinzenta
+        printf("está certa2");
+    }
+}
+}
+
+
+void procura(int **tabuleiro, int l1i, int c1i, int li, int ci, char tj[2]){
+int aux = tj[1] - 30, id[9]={},aux1;
+
+
+
+switch (aux)
+{
+case 2:
+
+    for (aux = -1; aux <= 1; aux++)
+    {
+        for (aux1 = -1; aux <= 1; aux1++, *id+=1)
+        {
+            id[0] = tabuleiro[l1i - aux][c1i - aux1];
+        }
+        
+        
+    }
+    *id+=-9;
+    
+    for (aux = 0; aux < 9; aux+=2)
+    {
+       if(id[aux] == 0){
+
+           //é vizinha branca
+
+       }else{
+
+           //não é vizinha branca
+       } 
+    }
+    
+
+    break;
+case 3:
+
+    for (aux = -1; aux <= 1; aux++)
+    {
+        for (aux1 = -1; aux <= 1; aux1++, *id+=1)
+        {
+            id[0] = tabuleiro[l1i - aux][c1i - aux1];
+        }
+        
+        
+    }
+    *id+=-9;
+    
+    for (aux = 0; aux < 9; aux+=2)
+    {
+       if(id[aux] > 0){
+
+           //é vizinha grey
+
+       }else{
+
+           //não é vizinha grey
+       } 
+    }
+    
+
+    break;
+case 4:
+    for (aux = -1; aux <= 1; aux++)
+    {
+        for (aux1 = -1; aux <= 1; aux1++, *id+=1)
+        {
+            id[0] = tabuleiro[l1i - aux][c1i - aux1];
+        }
+        
+        
+    }
+    *id+=-9;
+    
+    for (aux = 0; aux < 9; aux+=2)
+    {
+       if(id[aux] == -1){
+
+           //é vizinha negra
+
+       }else{
+
+           //não é vizinha negra
+       } 
+    }
+    
+
+    break;
+
+default:
+
+    break;
+}
+
+
+}
+
+
+void FazerFicehiro(){}
+int main(int argc, char *argv[])
 {
 
     /*l-linha, c-coluna, c1-Primeira Cordenada, c2-Segunda Cordenada, tj-Tipo de Jogo, np-Número de paças, tp- Tipo de Peça */
     /*char *l = NULL, *c= NULL, *c1= NULL, *c2= NULL, *tj= NULL, *np= NULL, *tp= NULL;*/
     int li = 0, ci = 0, c1i = 0, c2i = 0, npi = 0, tpi = 0, i = 0, **tabuleiro;
     char tj[2];
-    int opt = 0, flag, aux = 0, aux1=0;
-    char filename[] = "";
+    int opt = 0, flag, aux = 0, aux1=0,loop=1;
+    char filename[40] = {""};
 
-    FILE *fp;
-    /*ler ficheiro*/
-
-    if ((fp = fopen(filename, "r")) == NULL)
+    FILE *fp; /*ler ficheiro*/
+    
+    printf("teste again");
+    /*if ((fp = fopen(filename, "r")) == NULL)
     {
-        exit(0);
-    }
+        printf("porra");
+        exit(-1);
+    }*/
 
-    while (opt = getopt(argc, argv, "s:"))
-    {
+    while ((opt = getopt(argc, argv, "s:")) != -1)
+    {   printf("sexo");
         switch (opt)
         {
         case 's':
@@ -35,72 +162,44 @@ int main(int argc, char *argv)
             break;
         }
     }
-
+    printf("oi");
+    
+    fp = fopen(filename, "r");
     while (1)
-    {
-        aux = fscanf(fp, " %d %d %d %d %s %d", li, ci, c1i, c2i, tj, npi);
-        if (aux = !6)
+    {   printf("oi2\n");
+        aux = fscanf(fp, " %d %d %d %d %s %d", &li, &ci, &c1i, &c2i, tj, &npi);
+        if ((aux != 6))
         {
+            printf("oi1\n");
             break;
         }
         else
-        {
-            /* li = atoi(l);
-            ci = atoi(c);
-            c1i = atoi(c1);
-            c2i = atoi(c2);
-            npi = atoi(np); */
+        {   printf("dshbfzajn\n");
             aux=li;
             aux1=ci;
-            tabuleiro = (int **)calloc(li, sizeof(int *));
+           tabuleiro = (int **)calloc(li, sizeof(int *));
             for (i = 0; i < li; ++i)
             {
                 tabuleiro[i] = (int *)calloc(ci, sizeof(int));
             }
-            continue;
         }
         for (i = 0; i < npi; i++)
-        {
-            fscanf(fp, "%d %d %d", li, ci, tpi);
-            /*li = atoi(l);
-            ci = atoi(c);
-            tpi = atoi(tp);*/
+        {   
+            fscanf(fp, "%d %d %d", &li, &ci, &tpi);
+            printf("%d %d %d\n", li, ci, tpi);
             tabuleiro[li - 1][ci - 1] = tpi;
         }
+    A1(tabuleiro, c1i, c2i, li, ci);
     li = aux;
     ci = aux1;
+    //free(tabuleiro);
     }
-
+    
+    printf("\nola");
+    
+    //exit(0);
     fclose(fp);
-}
-
-void A1(int **tabuleiro, int l1i, int c1i, FILE *fp, int li, int ci){
-    int valor = tabuleiro[l1i][c1i];
-    //ver se pertence ao tabuleiro
-    if(l1i-1 < 0 || c1i-1 < 0 || l1i > li || c1i>ci ){
-        //Não pertence no fodendo tabuleiro
-    }else{
-    
-    if(valor<0){
-        //peça será preta
-    }else if(valor==0){
-        //peça branca
-    }else{
-        //peça cinzenta
-    }
-}
-}
-//Procurar o tipo de peça, ou seja, a2/a3/a4
-void procura(int **tabuleiro, int l1i, int c1i, int li, int ci, char tj[2]){
-
-    
+    printf("bomdia");
 
 
 }
-
-
-
-    /*for (i = 0; i < c; i++)
-        tabuleiro[i] = (int*)calloc(c,sizeof(int));*/
-
-/*A6 = Se ponto de partida - branca ->inicializar a -2. Correr algoritmo de procura e escolher uma peça branca. Ponto de chegada? Não então colocar -2. Repetir processo até ponto de chegada. Chega ao ponto de chegada, usar variavel auxiliar para saber que podem estar na mesma sala. Exit.*/
