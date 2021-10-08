3ff868e216114db9aa529938f6d2978cc789199c
Rodrigo Pereira <73695255+ErreEne@users.noreply.github.com>
2021-10-08 18:06:40 +0100

    olá bom dia a todos
    Ignora os printf's ha ha ha

--- main.c
+++ main.c
@@ -13,12 +13,13 @@ Ponto de chegada? Não então colocar -2. Repetir processo até ponto de chegada
 Chega ao ponto de chegada, usar variavel auxiliar para saber que podem estar na mesma sala. Exit.*/
 
 void A1(int **tabuleiro, int l1i, int c1i, int li, int ci){
-    int valor = tabuleiro[l1i-1][c1i-1];
     //ver se pertence ao tabuleiro
-    if(l1i-1 < 0 || c1i-1 < 0 || l1i > li || c1i>ci ){
+    if(l1i-1 < 0 || c1i-1 < 0 || l1i-1 > li || c1i-1>ci ){
         //Não pertence no fodendo tabuleiro
+        printf("mete essa merda no tabuleiro\n");
+        //exit(0);
     }else{
-    
+    int valor = tabuleiro[l1i-1][c1i-1];
     if(valor==-1){
         //peça será preta
         printf("está certa1");
@@ -33,25 +34,36 @@ void A1(int **tabuleiro, int l1i, int c1i, int li, int ci){
 }
 
 
-void procura(int **tabuleiro, int l1i, int c1i, int li, int ci, char tj[2]){
-int aux = tj[1] - 30, id[9]={},aux1;
+void procura(int **tabuleiro, int l1i, int c1i, int li, int ci, char tj[3]){
 
+int aux = tj[1] - 48, id[9]={0},aux1, i=0;
 
 
 switch (aux)
 {
 case 2:
-
+    printf("entrei");
+    //exit(0);
+    if(l1i-1 < 0 || c1i-1 < 0 || l1i-1 > li || c1i-1>ci ){
+        //Não pertence no fodendo tabuleiro
+        printf("mete essa merda no tabuleiro\n");
+        exit(0);
+    }else{
     for (aux = -1; aux <= 1; aux++)
-    {
-        for (aux1 = -1; aux <= 1; aux1++, *id+=1)
+    {   if(l1i-1 < 0 || c1i-1 < 0 || l1i-1 > li || c1i-1>ci ){
+        //Não pertence no fodendo tabuleiro
+        printf("mete essa merda no tabuleiro\n");
+        //exit(0);
+    }else{
+        for (aux1 = -1; aux <= 1; aux1++, i++)
         {
-            id[0] = tabuleiro[l1i - aux][c1i - aux1];
+            id[i] = tabuleiro[l1i - aux][c1i - aux1];
         }
         
-        
+    }    
     }
-    *id+=-9;
+    }printf("sad");
+    exit(0);
     
     for (aux = 0; aux < 9; aux+=2)
     {
@@ -135,9 +147,8 @@ int main(int argc, char *argv[])
 {
 
     /*l-linha, c-coluna, c1-Primeira Cordenada, c2-Segunda Cordenada, tj-Tipo de Jogo, np-Número de paças, tp- Tipo de Peça */
-    /*char *l = NULL, *c= NULL, *c1= NULL, *c2= NULL, *tj= NULL, *np= NULL, *tp= NULL;*/
-    int li = 0, ci = 0, c1i = 0, c2i = 0, npi = 0, tpi = 0, i = 0, **tabuleiro;
-    char tj[2];
+    int li = 0, ci = 0, c1i = 0, c2i = 0, npi = 0, tpi = 0, i = 0, **tabuleiro,tja;
+    char tj[3]={""};
     int opt = 0, flag, aux = 0, aux1=0,loop=1;
     char filename[40] = {""};
 
@@ -166,10 +177,9 @@ int main(int argc, char *argv[])
     
     fp = fopen(filename, "r");
     while (1)
-    {   printf("oi2\n");
-        aux = fscanf(fp, " %d %d %d %d %s %d", &li, &ci, &c1i, &c2i, tj, &npi);
-        if ((aux != 6))
-        {
+    {   //printf("oi2\n");
+        if ((fscanf(fp, "%d %d %d %d %s %d", &li, &ci, &c1i, &c2i, tj, &npi)) != 6)
+        {   //printf("%c",tj[1] );
             printf("oi1\n");
             break;
         }
@@ -186,20 +196,32 @@ int main(int argc, char *argv[])
         for (i = 0; i < npi; i++)
         {   
             fscanf(fp, "%d %d %d", &li, &ci, &tpi);
-            printf("%d %d %d\n", li, ci, tpi);
+            //printf("%d %d %d\n", li, ci, tpi);
             tabuleiro[li - 1][ci - 1] = tpi;
         }
-    A1(tabuleiro, c1i, c2i, li, ci);
+    tja = tj[1]-48;
+    printf("\n%d\n", tja);
+    switch (tja)
+    {
+    case 1:
+        A1(tabuleiro, c1i, c2i, li, ci);
+        break;
+    
+    case 2:
+        printf("sheesh\n");
+        procura(tabuleiro,c1i, c2i, li, ci, tj);
+        break;
+    default:
+        break;
+    }
     li = aux;
     ci = aux1;
     //free(tabuleiro);
     }
     
     printf("\nola");
-    
     //exit(0);
     fclose(fp);
-    printf("bomdia");
 
 
 }
