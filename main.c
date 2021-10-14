bb993cd25dd11412fbed79b611a62eeeb4678a23
Rodrigo Pereira <73695255+ErreEne@users.noreply.github.com>
2021-10-14 14:13:27 +0100

    dor de cabeça

--- main.c
+++ main.c
@@ -4,17 +4,12 @@
 #include <string.h>
 #include <getopt.h>
 
-    /*for (i = 0; i < c; i++)
-        tabuleiro[i] = (int*)calloc(c,sizeof(int));*/
 
-/*A6 = Se ponto de partida - branca ->inicializar a -2. 
-Correr algoritmo de procura e escolher uma peça branca. 
-Ponto de chegada? Não então colocar -2. Repetir processo até ponto de chegada. 
-Chega ao ponto de chegada, usar variavel auxiliar para saber que podem estar na mesma sala. Exit.*/
 
 int A1(int **tabuleiro, int l1i, int c1i, int li, int ci){
     //ver se pertence ao tabuleiro
     if(l1i-1 < 0 || c1i-1 < 0 || l1i-1 > li-1 || c1i-1>ci-1 ){
+
         return -2;
     }
     else{
@@ -48,7 +43,7 @@ if (l1i - 1 < 0 ||c1i -1 < 0 || c1i > ci || l1i > li)
     {
         id[i] = -2;
     }
-    /*Vai guardar todas as peças numa array*/
+    /*Vai guardar todas as peças à volta numa array*/
     for (aux = -1, i = 0; aux <= 1; aux++){
         if( l1i + aux - 1 < 0){
             aux++;
@@ -61,17 +56,15 @@ if (l1i - 1 < 0 ||c1i -1 < 0 || c1i > ci || l1i > li)
         {
             if(c1i + aux1 - 1 < 0 ){
             }else if(c1i + aux1 - 1 > ci - 1){
-                printf("%d %d %d %d\n", l1i-1 + aux, c1i-1 + aux1, li-1, ci-1);
                 break;
                 }else{
-            printf("%d ", tabuleiro[l1i-1+aux][c1i-1+aux1]);
+
             id[i] = tabuleiro[l1i - 1 + aux][c1i - 1 + aux1];
         }
         }
 
     }    
 aux = tj[1] - 48;
-printf("Aux = %d\n", aux);
 switch (aux)
 {
 case 2:
@@ -80,7 +73,6 @@ case 2:
     for (aux = 1; aux < 9; aux+=2)
     {
        if(id[aux] == 0){
-           printf("ola");
            return 1;
 
        }else{
@@ -93,8 +85,7 @@ case 2:
 case 3:
     for (aux = 1; aux < 9; aux+=2)
     {
-       if(id[aux] == 0){
-           printf("ola");
+       if(id[aux] > 0){
            return 1;
 
        }else{
@@ -107,8 +98,7 @@ case 3:
 case 4:
     for (aux = 1; aux < 9; aux+=2)
     {
-       if(id[aux] == 0){
-           printf("ola");
+       if(id[aux] == -1){
            return 1;
 
        }else{
@@ -130,30 +120,69 @@ default:
     break;
 }
 
-return -1;
+return 0;
 }
+void A6(int *id, int li, int ci, int l1i, int c1i, int l2i, int c2i){
+int aux, aux1, *sz;
+/*while(1){
+/*for(aux=-ci; aux<=ci; aux+=ci)
+{
+    if(0==aux){
 
-void FazerFicehiro(){}
+    if ((aux%ci != 0 || aux%(ci-1) != 0)/*condição de não ser na ponta)
+    /*{
+        for (aux1 = -1; aux1 <= 1; aux+=2)
+        {
+            if (id[(l1i-1)*ci + c1i + aux1] == 0)
+            {
+                /* conectar(?) e mudar o c1i e l1i */
+           /* }
+            
+        }
+        
+    }
+    
+
+    }else if(aux>0){
+
+    }else if(aux<0){
+
+    }
+}*/
+/*}*/
+}
+/*void FazerFicheiro(){
+
+char *filename;
+
+filename =  (char*) malloc (sizeof(char) * (strlen(ficheiro) + strlen(".sol1") + 1));
+
+strcat(filename, ".sol1");
+    
+}*/
 int main(int argc, char *argv[])
 {
 
     /*l-linha, c-coluna, c1-Primeira Cordenada, c2-Segunda Cordenada, tj-Tipo de Jogo, np-Número de paças, tp- Tipo de Peça */
-    int li = 0, ci = 0, c1i = 0, c2i = 0, npi = 0, tpi = 0, i = 0, tja, solu;
+    int li = 0, ci = 0, c1i = 0, l1i = 0, npi = 0, tpi = 0, i = 0, tja, solu, *id, l2i = 0, c2i = 0;
     int **tabuleiro;
     char tj[3]={""};
     int opt = 0, flag, aux = 0, aux1=0,loop=1;
     char filename[40] = {""};
+
+    // char *filename;
+    //filename = (char* ) malloc(strlen(argv[2]) * sizeof(char) +1);
 
     FILE *fp; /*ler ficheiro*/
     
     /*if ((fp = fopen(filename, "r")) == NULL)
     {
         printf("porra");
-        exit(-1);
+        exit(0);
     }*/
 
     while ((opt = getopt(argc, argv, "s:")) != -1)
-    {   printf("sexo");
+    {   
         switch (opt)
         {
         case 's':
@@ -165,46 +194,57 @@ int main(int argc, char *argv[])
             
         }
     }
-    printf("oi");
+
     
     fp = fopen(filename, "r");
     if ((fp = fopen(filename, "r")) == NULL)
     {
-        printf("porra");
-        exit(-1);
+        //exit(0);
     }
-
     while (1)
-    { 
+    {
+        aux = fscanf(fp, "%d %d %d %d %s", &li, &ci, &l1i, &c1i, tj);
+        if(aux!=5)break;
+
         //alterar este metodo com o fscanf para ver quando ficheiro está NULL porque como está agora vamos ter probleminhas no valgrind
-        if ((fscanf(fp, "%d %d %d %d %s %d", &li, &ci, &c1i, &c2i, tj, &npi)) != 6)
-        {   
-            break;
+        tja = tj[1]-48;
+        //exit(0);
+        if (tja == 6)
+        {
+        tja = li;
+        aux1 = ci;
+        fscanf(fp,"%d %d %d", &l2i, &c2i, &npi );
+        id = (int*)calloc(ci,sizeof(int));
+        for(aux=0; aux<npi; aux++){
+            fscanf(fp, "%d %d %d", &li, &ci, &tpi);
+            id[(li-1)*10 + ci-1] = tpi;
         }
-        else
-        {   
+        
+ 
+        }else{
+        
+            fscanf(fp, "%d", &npi);
             aux=li;
             aux1=ci;
             tabuleiro = (int **)calloc(li, sizeof(int *));
             for (i = 0; i < li; ++i)
             {
                 tabuleiro[i] = (int *)calloc(ci, sizeof(int));
+
             }
-        }
+            
         for (i = 0; i < npi; i++)
-        {   
+        {  
             fscanf(fp, "%d %d %d", &li, &ci, &tpi);
             tabuleiro[li - 1][ci - 1] = tpi;
         }
     
-    tja = tj[1]-48;
-    printf("%d", tabuleiro[9][9]);
     li = aux;
     ci = aux1;
     switch (tja)
     {
     case 1:
-        solu = A1(tabuleiro, c1i, c2i, li, ci);
+        solu = A1(tabuleiro, l1i, c1i, li, ci);
         printf("solu = %d", solu);
         break;
     
@@ -212,20 +252,22 @@ int main(int argc, char *argv[])
     case 3:
     case 4:
     case 5:
-        solu = procura(tabuleiro,c1i, c2i, li, ci, tj);
+        solu = procura(tabuleiro,l1i, c1i, li, ci, tj);
         printf("solu = %d", solu);
         break;
     default:
         break;
     }
     
-    /*for (i = 0; i < ci; i++)
+    
+    }
+    for (i = 0; i < li; i++)
     {
         free(tabuleiro [i]);
     }
     
     free(tabuleiro);
-    }*/
+    
     }
     fclose(fp);
 
