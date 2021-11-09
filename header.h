17d38c1bf4fbb0e6f1d19be9bd1bcdeadb11459c
Rafaela Oliveira <80894252+rafaelacorreiaoliveira@users.noreply.github.com>
2021-11-09 22:28:30 +0000

    Update header.h

--- header.h
+++ header.h
@@ -22,7 +22,7 @@ typedef struct grafo Grafo;
 Grafo *criargarph(int salas, Grafo *estr);
 void montargraph(Grafo *estr, int salas, int vertice1, int vertice2);
 void grafonightmare(Grafo *estr, int salas, int **tabuleiro, int li, int ci);
-void criarligacao(Grafo *estr, int vertice1, int vertice2, Item custo);
+void criarligacao(Grafo *estr, int vertice1, int vertice2, int custo);
 int compareItems(int it1, int it2);
 
 /*Funções Algoritmo Dijkstra*/
